caafc3f3b3045559309f4a1ad1d34d4d38854297
unknown <mstiff1983@gmail.com>
2020-05-17 17:40:22 -0400

    I have added a list of 3 dinosaurs to the herd class

--- RobotsvsDinosaurs/Herd.cs
+++ RobotsvsDinosaurs/Herd.cs
@@ -8,5 +8,7 @@ namespace RobotsvsDinosaurs
 {
     class Herd
     {
+        public List<Dinosaur> dinosaurs = new List<Dinosaur>() { new Dinosaur("TRex"), new Dinosaur("Spino"), new Dinosaur("Plateo") };
+
     }
 }
