2cd998a247b0c9462d52ab529e1d852d293bd17d
unknown <mstiff1983@gmail.com>
2020-05-17 22:36:49 -0400

    I added an SelectAttackType method with a switch case to the Dinosaur class to allow the dinosaur to select its attack type

--- RobotsvsDinosaurs/Dinosaur.cs
+++ RobotsvsDinosaurs/Dinosaur.cs
@@ -27,7 +27,33 @@ namespace RobotsvsDinosaurs
             energy -= 10;
 
         }
+    public void SelectAttackType()
+        {
+            Console.WriteLine("Please Select your Attack Type: Dragon Breath, Tail Spin, Bite");
+            string attackType = Console.ReadLine();
+
+            switch (attackType)
+            {
+                case "Dragon Breath":
+                    Console.WriteLine("You have selected Dragon Breath");
+                    break;
+                case "Tail Spin":
+                    Console.WriteLine("You have selected Tail Spin");
+                    break;
+                case "Bite":
+                    Console.WriteLine("You have selected Bite");
+                    break;
+                default:
+                    Console.WriteLine("This is an invalid selection");
+                    break;
+
+
+
+            }
+
+        }
     }
+
 
 
    
